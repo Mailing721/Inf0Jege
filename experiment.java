1781e8ebd30534e11282c5558768f1a7baa9a518
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2023-12-30 20:19:46 +0300

    Add files via upload

---
+++ experiment.java
@@ -0,0 +1,19 @@
+public class Main{
+	public static void main(String[] args){
+		int x;
+		int y;
+		int z;
+		int w;
+		for (x=0; x <= 1; x++){
+		    for (y=0; y <= 1; y++){
+		        for (z=0; z <= 1; y++){
+                    for (w=0; w <= 1; w++){
+                        if (x+y+z+w == 1){
+                            System.out.println(x+y+z+w);
+                        }		    
+		}		    
+		}
+		}
+		}
+		}
+	}
