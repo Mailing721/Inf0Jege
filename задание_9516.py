5d065b8078d3d8d8541b55ab7bcd925f19bea356
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-02-17 18:15:22 +0300

    Add files via upload

--- задание_9516.py
+++ задание_9516.py
@@ -2,7 +2,7 @@ a=open('09_9156.csv').readlines()
 k=0
 for i in a:
     s=[int(x) for x in i.strip().split(';')]
-    if (min(s)+max(s))%3==0 and (abs(s[0]-s[1])==abs(s[2]-s[3]) or abs(s[0]-s[2])==abs(s[1]-s[3])):
+    if (min(s)+max(s))%3==0 and (abs(s[0]-s[1])==abs(s[2]-s[3]) or abs(s[0]-s[2])==abs(s[1]-s[3]) or abs(s[0]-s[3])==abs(s[1]-s[2])):
         k+=1
 
 print(k)
